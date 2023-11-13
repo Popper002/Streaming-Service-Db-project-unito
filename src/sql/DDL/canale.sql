d6a8738c200c7e7a7c83883922dea3b8be4eb68b
Riccardo Oro <riccardo.oro@edu.unito.it>
2023-11-13 15:34:33 +0100

    queary correction 1

--- src/sql/DDL/canale.sql
+++ src/sql/DDL/canale.sql
@@ -1,28 +1,29 @@
 CREATE TABLE IF NOT EXISTS Canale
 (
     nickname varchar(20),
+	PRIMARY KEY (nickname),
     iscritti integer DEFAULT 0,
     social_media_list varchar(45),
-    biografia varchar(280), 
-    PRIMARY KEY nickname
+    biografia varchar(280)
+
 );
 
-CREATE TYPE contents AS ENUM ('live', 'video', 'clip');
+--CREATE TYPE contents AS ENUM ('live', 'video', 'clip');
 
 CREATE TABLE Contiene (
     id SERIAL PRIMARY KEY,
     media_name VARCHAR(255),
     contents contents,
-    duration_seconds INTEGER,
+    duration_seconds FLOAT,
     content contents,
     canale_nickname varchar(20),
     CONSTRAINT to_catalog CHECK (
-        content = CASE 
+        content = CASE
                 WHEN duration_seconds > 60 then 'video'
                 ELSE 'clip'
             END
     ),
-    CONSTRAINT fk_canale
+        CONSTRAINT fk_canale
         FOREIGN KEY (canale_nickname)
         REFERENCES Canale(nickname)
         ON DELETE CASCADE
--- src/sql/DDL/contenuti.sql
+++ src/sql/DDL/contenuti.sql
@@ -4,4 +4,9 @@ CREATE TABLE IF NOT EXISTS contenuti(
     clip integer default 0,
     url varchar(45),
     PRIMARY KEY ( url)
+     constraint check_url check (
+        url *~ '((http|https)://)(www.)?” 
++ “[a-zA-Z0-9@:%.\+~#?&//=]{2,256}\.[a-z]” 
++ “{2,6}\b([-a-zA-Z0-9@:%.\+~#?&//=]*)'
+    )
 ); 
