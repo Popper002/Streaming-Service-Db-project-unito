21a030cb7a8b35005dfb93df150ad23aab2edd30
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-11-13 14:32:58 +0100

    test , check this thing plz

--- src/sql/DDL/canale.sql
+++ src/sql/DDL/canale.sql
@@ -1,10 +1,30 @@
 CREATE TABLE IF NOT EXISTS Canale
 (
-    channel_name varchar(45)  NOT NULL,
-    nickname varchar(20)
+    nickname varchar(20),
     iscritti integer DEFAULT 0,
-    contenuti integer DEFAULT 0,
-    social_media_list character varying(45),
-    biografia character varying(280) 
+    social_media_list varchar(45),
+    biografia varchar(280), 
     PRIMARY KEY nickname
+);
+
+CREATE TYPE contents AS ENUM ('live', 'video', 'clip');
+
+CREATE TABLE Contiene (
+    id SERIAL PRIMARY KEY,
+    media_name VARCHAR(255),
+    contents contents,
+    duration_seconds INTEGER,
+    content contents,
+    canale_nickname varchar(20),
+    CONSTRAINT to_catalog CHECK (
+        content = CASE 
+                WHEN duration_seconds > 60 then 'video'
+                ELSE 'clip'
+            END
+    ),
+    CONSTRAINT fk_canale
+        FOREIGN KEY (canale_nickname)
+        REFERENCES Canale(nickname)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
---
+++ src/sql/DML/population_canale.sql
@@ -0,0 +1,3 @@
+INSERT INTO Canale(chanel_name,nickname,iscritti,contenuti,social_media_list,biografia)
+(
+);
