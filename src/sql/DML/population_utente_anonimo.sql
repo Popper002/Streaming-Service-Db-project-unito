f79210455cfebcb5f36ac4550c2ac45d8c16c8ee
Riccardo Oro <riccardo.oro@edu.unito.it>
2023-11-13 15:53:05 +0100

    population anonimo some data

---
+++ src/sql/DML/population_utente_anonimo.sql
@@ -0,0 +1,37 @@
+INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '192.168.0.1'
+);
+INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '192.163.0.1'
+);
+INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '555.168.0.2'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '111.168.0.1'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '196.168.0.1'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '192.168.1.3'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '192.168.4.7'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '191.111.9.4'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '192.109.6.9'
+);INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '000.000.0.0'
+);
+INSERT INTO Utente_anonimo(ip_address )
+VALUES(
+    '333.33333.1.333'
+);
