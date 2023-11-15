d142a81cb3cf21841dd3683d00b857323b119037
Riccardo Oro <riccardo.oro@edu.unito.it>
2023-11-15 19:36:14 +0100

    da perfezionare cose

--- src/sql/DML/operazioni.sql
+++ src/sql/DML/operazioni.sql
@@ -16,8 +16,18 @@ WHERE nickname IN (SELECT nickname FROM IsAffiliate);
 
 --@block Calcolo 1/settimana classifica streamer più seguiti
 WITH Ranking as(
-  select nickname, iscritti, RANK() OVER (ORDER BY iscritti DESC) AS rank
+  select nickname, iscritti, RANK_NUMBER() OVER (ORDER BY iscritti DESC) AS rank_streamer --forse ;'
   from Canale
 
   GROUP BY iscritti where MAX(iscritti)  
 ) -- TODO: da riguardare non mi convince 
+
+--delete from calendario
+  --  where titolo_futuro ~ '^[0-9].*$')--espressione regolare che ha sequenza di numeri con un . e fine corsa $
+  
+  --si può fare anche cosi usando l'identificativo per numeri \d
+
+  DELETE FROM calendario WHERE titolo_futuro ~'^\d.*$' --non voglio avere titoli solo con numeri 
+  --cosi evitiamo le seguenti casistiche . -inserimento numero di telefono in campo errato. -un ip_adress o altro nel campo.ADD
+--documentation for regular expression postgresql https://www.sqlshack.com/working-with-regular-expressions-in-postgresql/
+  
