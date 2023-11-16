611352a3e71319ef0176c937d69e9bd9c0f4491c
Riccardo Oro <riccardo.oro@edu.unito.it>
2023-11-16 16:28:53 +0100

    commit

--- src/sql/DML/inserimenti.sql
+++ src/sql/DML/inserimenti.sql
@@ -5,3 +5,5 @@ INSERT INTO calendario(titolo_futuro ,iniziio)
 VALUES('LIVE FUTURISTICA','20/12/2021'); 
 INSERT INTO chat(pubblica,messaggio,nick_mandante,nick_ricevente,tempo)
 VALUES(FALSE,'ciaoquestoèunmessaggio','riccardo','francesco',CURRENT_TIMESTAMP);
+
+INSERT INTO chat()
